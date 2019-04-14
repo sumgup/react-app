d245779e2daa7ce8073c08b24cb3145999f30f9a
Dhananjay kumar <dhananjay.kumar@neudesic.com>
2019-04-14 22:02:14 +0530

    mpnths comp

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Clock from "./Components/clock";
 import NavComp from "./Components/navComp";
 import Scheduler from "./Components/Scheduler";
+import Month from "./Components/Month";
 
 class App extends Component {
   
@@ -26,7 +27,12 @@ class App extends Component {
             <div className="col-sm border bg-light">
                 <Scheduler/>
             </div>                  
-        </div>        
+        </div> 
+        <div className = "row">
+            <div className="col-sm border bg-light">
+                <Month/>
+            </div>                  
+        </div>          
       </div>    
     );
   }
