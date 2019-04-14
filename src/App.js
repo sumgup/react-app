d22d5d6c9d2e49d26c8e99de9d174790817ece2e
Dhananjay kumar <dhananjay.kumar@neudesic.com>
2019-04-14 21:51:54 +0530

    added scheduker

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import Clock from "./Components/clock";
 import NavComp from "./Components/navComp";
+import Scheduler from "./Components/Scheduler";
 
 class App extends Component {
   
@@ -19,6 +20,11 @@ class App extends Component {
         <div className = "row">
             <div className="col-sm border bg-light">
                 <NavComp/>
+            </div>                  
+        </div>
+        <div className = "row">
+            <div className="col-sm border bg-light">
+                <Scheduler/>
             </div>                  
         </div>        
       </div>    
